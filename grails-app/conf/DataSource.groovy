4c6407fee31a6f8c7839a0479dfa713aad4ebfca
Cyrus Malekpour <cmalekpour@gmail.com>
2014-06-09 13:20:11 -0400

    Switched back to in memory database, with MySQL configuration still left in but commented out

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,14 @@
-dataSource {
+/*dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
     dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
     username = "root"
+}*/
+dataSource {
+    pooled = false
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
 }
 
 hibernate {
@@ -17,17 +23,20 @@ hibernate {
 environments {
     development {
         dataSource {
-            url = "jdbc:mysql://localhost/grailsgoat-dev?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
+            //url = "jdbc:mysql://localhost/grailsgoat-dev?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
+            url = "jdbc:h2:devDb"
         }
     }
     test {
         dataSource {
-            url = "jdbc:mysql://localhost/grailsgoat-test?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
+            //url = "jdbc:mysql://localhost/grailsgoat-test?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
+            url = "jdbc:h2:devDb"
         }
     }
     production {
         dataSource {
-            url = "jdbc:mysql://localhost/grailsgoat-prod?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
+            //url = "jdbc:mysql://localhost/grailsgoat-prod?useUnicode=yes&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull"
+            url = "jdbc:h2:devDb"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
