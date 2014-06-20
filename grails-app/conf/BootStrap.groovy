ae92730dc4dee5d361864489363e262948123d28
John Poulin <john.m.poulin@gmail.com>
2014-06-19 21:14:29 -0400

    Finished creating basic seed data for H2

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,9 +1,13 @@
 import grails.nV.Company
 import grails.nV.JobListing
+import grails.nV.User
+import grails.nV.Message
 
 class BootStrap {
 
     def init = { servletContext ->
+
+    	if(!Company.count()) {
     		def leetsec = new Company(name: "LeetSec", description: "Experts in Application Security", website: "https://leetsec.com" ).save(failOnError: true)
     		def megacorp = new Company(name: "MegaCorp", description: "The best at what we do", website: "http://megacorp.co.jp").save(failOnError: true)
     		def goatsllc = new Company(name: "GoatS LLC", description: "We provide the best goats, for traditional goat stew", website: "http://goatsforstew.com").save(failOnError: true)
@@ -12,6 +16,11 @@ class BootStrap {
     		def job2 = new JobListing(name: "Senior Security Consultant", description: "Manages the rookies", requirements: "3+ years experience in application security. Extensive Grails Experience", howtoapply: "Contact our HR team: megangoat@leetsec.com", location: "Virginia", startdate: new Date(), fulltime: true, company: leetsec).save(failOnError: true)
     		def job3 = new JobListing(name: "Financial Assistant", description: "Responsible for managing financial tasks, such as investments and purchasing", requirements: "Extensive experience with MS Excel + Access", howtoapply: "Please give us a call at (211) 355-3413 x1337. Ask for Cyrus", location: "Virginia", startdate: new Date(), fulltime: true, company: megacorp).save(failOnError: true)
 
+    		def admin = new User(email: "admin@grails.com", firstname: "Admin", lastname: "Admin", fullname: "Admin Admin", password: "password".encodeAsMD5(), verify_token: null, accesslevel: 1).save(failOnError: true)
+    		def john = new User(email: "john@grails.com", firstname: "John", lastname: "Poulin", fullname: "John Poulin", password: "haX0r!".encodeAsMD5(), verify_token: null, accesslevel: 2).save(failOnError: true)
+
+    		def message = new Message(subject: "Help with job", body: "Hello Admin, I'm currently looking for help finding a job. Please give me a call, (222) 345-9241", author_id: 2, recipient: admin).save(failOnError: true)
+  	  }
     }
     def destroy = {
     }
